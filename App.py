65e7bef2884faa1d8c11e89016684359b4a4790f
[vishal vinod] <[nikkabenconson@gmail.com]>
2025-06-06 02:11:31 +0530

    image1 commit

--- App.py
+++ App.py
@@ -70,7 +70,7 @@ elif app_mode == "Prediction":
     st.header("Model Prediction")
     test_image = st.file_uploader("Choose an Image:")
     if st.button("Show Image") and test_image is not None:
-        st.image(test_image, width=4, use_container_width=True)
+        st.image(test_image, width=4, use_column_width=True)
     # Predict button
     if st.button("Predict") and test_image is not None:
         st.snow()
